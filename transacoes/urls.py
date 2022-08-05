89bdabffb5d45038bf4c9e59e67163c5d836fc19
Nicholas Baú <baunbd@gmail.com>
2022-08-05 03:05:06 -0300

    Removendo trechos comentados

--- transacoes/urls.py
+++ transacoes/urls.py
@@ -8,24 +8,3 @@ urlpatterns = [
     path('despesas/', views.DespesasList.as_view()),
     path('despesas/<int:pk>/', views.DespesaDetail.as_view()),
 ]
-
-'''
-router = routers.SimpleRouter()
-
-router.register(r'receitas', views.Receitas.as_view())
-router.register(r'despesas', views.Despesas.as_view())
-
-urlpatterns = [
-    path("", include(router.urls)),
-]
-'''
-
-'''
-urlpatterns = [
-    path('receitas/', views.receita, name='receita'),
-    path('receitas/<str:id>/', views.detalha_receita, name='detalha_receita'),
-    path('despesas/', views.despesa, name='despesa'),
-    path('', include(router.urls)),
-    #path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
-'''
