d309fe4635d339819dd1d251d0d8dcadeb600e7d
Nicholas Baú <baunbd@gmail.com>
2022-08-15 22:49:33 -0300

    Semana 2: URL, Views, Serializers

--- transacoes/models.py
+++ transacoes/models.py
@@ -12,9 +12,20 @@ class Receitas(models.Model):
 
 
 class Despesas(models.Model):
+    CATEGORIA_CHOICES = (
+        ('1', 'Alimentação'),
+        ('2', 'Saúde'),
+        ('3', "Moradia"),
+        ("4", "Transporte"),
+        ("5", "Educação"),
+        ("6", "Lazer"),
+        ("7", "Imprevistos"),
+        ("8", "Outras"),
+    )
+
     descricao = models.CharField(max_length=200)
-    valor = models.DecimalField(
-        decimal_places=2, max_digits=15, null=False, blank=False)
+    valor = models.DecimalField(decimal_places=2, max_digits=15, null=False, blank=False)  # noqa
+    categoria = models.CharField(max_length=15, null=False, blank=False, choices=CATEGORIA_CHOICES, default='Outras')  # noqa
     data = models.DateField(null=False, blank=False)
 
     def __str__(self):
--- transacoes/serializers.py
+++ transacoes/serializers.py
@@ -9,6 +9,27 @@ class ReceitasSerializer(serializers.ModelSerializer):
 
 
 class DespesasSerializer(serializers.ModelSerializer):
+    # categoria = CategoriaField(source="*")
+
     class Meta:
         model = Despesas
-        fields = ('id', 'descricao', 'valor', 'data')
+        fields = ('id', 'descricao', 'valor', 'categoria', 'data')
+
+    # Se categoria não vier especificada, então assume-se que é Outras
+    def to_representation(self, instance):
+        dado = super().to_representation(instance)
+        if not dado.get('categoria'):
+            dado['categoria'] = 'Outras'
+        return dado
+
+
+class ReceitaListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Receitas
+        fields = ('descricao', 'valor',)
+
+
+class DespesaListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Despesas
+        fields = ('descricao', 'valor', 'categoria',)
